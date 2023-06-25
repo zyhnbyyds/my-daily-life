76687befe20e4f0bd4bf6ecdcdfe7c50036f962a
zhang771 <19939926438@163.com>
2023-06-25 20:05:52 +0800

    fix: waterfall show error

--- layouts/life.vue
+++ layouts/life.vue
@@ -11,53 +11,64 @@ const { data: lifeDataList } = await useAsyncData('lifeDataList', () => queryCon
 
 const { width } = useWindowSize()
 const activeCol = ref<number>(4)
-const img = useImage()
-const { cloned } = useCloned(lifeDataList.value)
-
-const backgroundStyles = computed(() => {
-  const imgUrl = img('https://github.com/nuxt.png', { width: 100 })
-  return { backgroundImage: `url('${imgUrl}')` }
-})
+judgeCols(width.value)
 
 watch(() => width.value, (newVal) => {
-  if (newVal > 1024)
-    activeCol.value = 4
-
-  else if (newVal >= 768 && newVal < 1024)
-    activeCol.value = 3
-
-  else if (newVal >= 640 && newVal < 768)
-    activeCol.value = 2
-
-  else if (newVal < 640)
-    activeCol.value = 2
+  judgeCols(newVal)
 })
 
-// TODO fix show bug
+function judgeCols(windowWidth: number) {
+  if (windowWidth > 1024)
+    activeCol.value = 4
+
+  else if (windowWidth >= 768 && windowWidth < 1024)
+    activeCol.value = 3
+
+  else if (windowWidth < 768)
+    activeCol.value = 2
+}
+
+// TODO => fix show bug
 const actShowList = computed(() => {
+  if (!lifeDataList.value || lifeDataList.value.length === 0)
+    return [[]]
+  const dataLength = lifeDataList.value.length
+
   const arr: LifeItem[][] = []
   for (let index = 0; index < activeCol.value; index++)
     arr.push([])
 
-  cloned.value?.forEach((item, index) => {
-    arr.forEach((_item, index2) => {
-      if ((index + 1) % (index2 + 1) === 0)
-        arr[index2].push(item as unknown as LifeItem)
+  const num = Math.floor(dataLength / activeCol.value)
+  for (let index = 0; index < num; index++) {
+    const arr2 = lifeDataList.value.slice(index * activeCol.value, (index + 1) * activeCol.value)
+    arr.map((item, index) => {
+      item.push(arr2[index] as unknown as LifeItem)
+      return item
     })
-  })
-
+  }
+  if (dataLength % activeCol.value !== 0) {
+    const num2 = dataLength % activeCol.value
+    const toPushArr = lifeDataList.value.slice(num * activeCol.value, dataLength)
+    for (let index = 0; index < num2; index++) {
+      arr.map((item, idx) => {
+        if (idx === index)
+          item.push(toPushArr[index] as unknown as LifeItem)
+        return item
+      })
+    }
+  }
   return arr
 })
 </script>
 
 <template>
   <main class="m-auto mt-2 lg:w-10/12 sm:w-full sm:px-4">
-    <div :style="backgroundStyles" />
     <div class="w-full px-3 lg:grid-cols-4 md:grid-cols-3" sm="grid-cols-2" grid="~ gap-4 cols-2">
       <div v-for="item, i in actShowList" :key="i">
+        <!-- {{ item }} -->
         <div v-for="item2, idx2 in item" :key="idx2" class="mb-4">
-          <NuxtLink :to="item2._path">
-            <NuxtImg format="webp" hover="scale-102" class="rounded-md shadow-md transition-transform" :src="item2.imgUrl" />
+          <NuxtLink :to="(item2 ?? {})._path || '/'">
+            <NuxtImg format="webp" hover="scale-102" class="grid-rows-2 rounded-md shadow-md transition-transform" :src="(item2 ?? {}).imgUrl || '/preview.jpg'" />
           </NuxtLink>
         </div>
       </div>
